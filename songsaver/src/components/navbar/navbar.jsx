27ea67fe9635e9300d95575784aeda1f8af959fb
LuMaxIe <max.lugthart@icloud.com>
2022-05-08 14:53:14 +0200

    Finished routing

--- songsaver/src/components/navbar/navbar.jsx
+++ songsaver/src/components/navbar/navbar.jsx
@@ -1,9 +1,11 @@
 import './navbar_styles.css'
+import { Link } from 'react-router-dom';
 
 export const NavContainer = () => {
   return (
     <div className="navbar">
       <img src='/winc-logo-2.png' alt='wincacademy logo' />
+      <Link to="/About" className='about-link'>About</Link>
     </div>
   )
 }
--- songsaver/src/index.js
+++ songsaver/src/index.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { store } from './redux/app/store';
 import App from './App';
 import './styles/index.css';
+import { About } from './routes/About';
 
 const container = document.getElementById('root');
 const root = createRoot(container);
@@ -11,7 +13,12 @@ const root = createRoot(container);
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<App />} />
+          <Route path="/About" element={<About />} />
+        </Routes>
+      </BrowserRouter>
     </Provider>
   </React.StrictMode>
 );
